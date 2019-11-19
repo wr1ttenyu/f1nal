6e21f52422a46ec0a40a281cd015a411593f2e0e
wr1ttenyu <826543564@qq.com>
2019-11-19 22:16:47 +0800

    single responsibility principle study finish and interface segregation principle study init

--- design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/principle/singleresponsibility/SingleResponsibility1.java
+++ design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/principle/singleresponsibility/SingleResponsibility1.java
@@ -15,11 +15,13 @@ public class SingleResponsibility1 {
 /**
  * 交通工具类
  * 方式1
- * 1. 在方式1的 run 方法中违反了单一职责原则 飞机不能再公路上跑
+ * 1. 在方式1的 run 方法中违反了单一职责原则
  * 2. 解决方式 建多个交通工具类 负责运行在不同领域的交通工具
  */
 class Vehicle {
     public void run(String vehicle) {
-        System.out.println(vehicle + "在公路上跑");
+        if("摩托车".equals(vehicle)) System.out.println(vehicle + "在公路上跑");
+        if("汽车".equals(vehicle)) System.out.println(vehicle + "在公路上跑");
+        if("飞机".equals(vehicle)) System.out.println(vehicle + "在天上飞");
     }
 }
--- design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/principle/singleresponsibility/Summary.java
+++ design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/principle/singleresponsibility/Summary.java
@@ -1,7 +1,13 @@
 package wr1ttenyu.f1nal.study.designpattern.principle.singleresponsibility;
 
 /**
+ * 单一职责原则注意事项和细节
  *
+ * 1. 降低类的复杂度，一个类只负责一项职责
+ * 2. 提高类的可读性，可维护性
+ * 3. 降低变更引起的风险
+ * 4. 通常情况下，我们应当遵守单一职责原则，只有逻辑足够简单，才可以在代码级违反单一职责原则；
+ *  只有类中方法数量足够少，可以在方法级别保持单一职责原则
  */
 interface Summary {
 }
