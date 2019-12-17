206755be7a3b67ecc7695cc94e2d46307ed6a494
xingyu.zhao <xingyu.zhao@bangdao-tech.com>
2019-12-17 18:06:36 +0800

    command design pattern study and some HashMap study

---
+++ design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/pattern23/command/LightReceiver.java
@@ -0,0 +1,12 @@
+package wr1ttenyu.f1nal.study.designpattern.pattern23.command;
+
+public class LightReceiver {
+
+    public void on() {
+        System.out.println("执行电灯打开命令，电灯亮了...");
+    }
+
+    public void off() {
+        System.out.println("执行电灯关系命令，电灯熄灭了...");
+    }
+}
