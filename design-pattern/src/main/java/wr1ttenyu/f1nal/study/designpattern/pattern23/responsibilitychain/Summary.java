53256bc5f82a59e82631ae4140ce7a23c31aee64
xingyu.zhao <xingyu.zhao@bangdao-tech.com>
2020-01-03 19:00:47 +0800

    netty study

--- design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/pattern23/responsibilitychain/Summary.java
+++ design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/pattern23/responsibilitychain/Summary.java
@@ -1,4 +1,11 @@
 package wr1ttenyu.f1nal.study.designpattern.pattern23.responsibilitychain;
 
+/**
+ * 职责链模式基本介绍
+ * 1. 职责链模式（Chain of Responsibility Pattern），又叫 责任链模式，为请求创建了一个接收者对象的链。
+ *      这种模式对请求的发送者和接收者进行解耦。
+ * 2. 职责链模式通常每个接收者都包含另一个接收者的引用。如果一个对象不能处理该请求，那么它会把相同的请求传给下一个接收者，以此类推。
+ * 3. 这种类型的设计模式属于行为型模式
+ */
 public class Summary {
 }
