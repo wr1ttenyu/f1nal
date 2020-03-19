45d95e158e57ae74ac412ebbc616ed1d6f2c1550
wr1ttenyu <826543564@qq.com>
2020-03-19 08:18:47 +0800

    some stuff

---
+++ javase/src/test/java/wr1ttenyu/f1nal/study/leetcode/CustomHashMap.java
@@ -0,0 +1,62 @@
+package wr1ttenyu.f1nal.study.leetcode;
+
+public class CustomHashMap {
+
+}
+
+class MyHashMap {
+
+    private String
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+
+    }
+}
+
+class MyHashMapNode {
+    private int key;
+
+    private int value;
+
+    private MyHashMapNode next;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public MyHashMapNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyHashMapNode next) {
+        this.next = next;
+    }
+}
