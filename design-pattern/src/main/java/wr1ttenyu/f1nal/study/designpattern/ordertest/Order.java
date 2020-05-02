ba20d411ed54102cbea6db62a624c366633423b5
wr1ttenyu <826543564@qq.com>
2020-05-02 09:21:46 +0800

    some stuff

--- design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/ordertest/Order.java
+++ design-pattern/src/main/java/wr1ttenyu/f1nal/study/designpattern/ordertest/Order.java
@@ -9,4 +9,36 @@ public class Order {
     private Long price;
 
     private OrderType orderType;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public OrderType getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(OrderType orderType) {
+        this.orderType = orderType;
+    }
 }
--- javase/src/main/java/wr1ttenyu/f1nal/study/javase/jvm/gc/HelloGC.java
+++ javase/src/main/java/wr1ttenyu/f1nal/study/javase/jvm/gc/HelloGC.java
@@ -6,11 +6,22 @@ import java.util.concurrent.TimeUnit;
 
 public class HelloGC {
 
+    private static byte[] test = new byte[1024 * 1024 * 12];
+
     // jinfo -flags [pid] 查看JVM所有初始配置项
     // jinfo -flag [flagName:PrintGCDetail] [pid]
     // jinfo -flag PrintGCDetails 16388
     // jinfo -flag MetaspaceSize 15756
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         /*System.out.println("Hello GC");
         try {
             TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
